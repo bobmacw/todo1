965efb3ed96cfbc85e5ccf4cfb46f6736e715e17
bobmacw <bobmacw@yahoo.com>
2015-07-10 17:27:26 -0400

    Cleanup

--- server.js
+++ server.js
@@ -30,13 +30,13 @@ var router = express.Router();
 // middleware to use for all requests
 router.use(function(req, res, next) {
 	// do logging
-	console.log('Something is happening.');
+	console.log('Request received');
 	next();
 });
 
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
 router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
+	res.json({ message: 'Todo API is online' });
 });
 
 // on routes that end in /bears
@@ -48,25 +48,18 @@ router.route('/bears')
 	// create a bear (accessed at POST http://localhost:8080/bears)
 	.post(function(req, res) {
 
-        console.log('in bears post');
 		var bear = new Bear();		// create a new instance of the Bear model
 		bear.name = req.body.name;  // set the bears name (comes from the request)
 
-        console.log('ready to save '+ bear.name);
 		bear.save(function(err) {
-            console.log('in save callback');
 			if (err) {
 
-                consoe.log('save err' + err);
+                console.log('save err' + err);
                 res.send(err);
             }
 
 			res.json({ message: 'Bear created!' });
-            console.log('done saving');
 		});
-        console.log('end of post');
-
-		
 	})
 
 	// get all the bears (accessed at GET http://localhost:8080/api/bears)
@@ -129,4 +122,4 @@ app.use('/api', router);
 // START THE SERVER
 // =============================================================================
 app.listen(port);
-console.log('Magic happens on port ' + port);
+console.log('Todo running on port ' + port);
